335f091f7dc535eda5b0a36c3b295516c9530067
Hitesh Sharma <hitesh@slideshare.com>
2015-01-30 00:29:34 +0530

    changed video

--- javascripts/video_controls.js
+++ javascripts/video_controls.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  video_tag = $(".bg_video").get(0);
   $(".bg_video").bind("ended", function(){
     v = this.src;
     this.src = '';
@@ -6,4 +7,18 @@ $(document).ready(function(){
     this.controls=false;
     this.src = v;
   });
+
+  $(".menu .marriage").bind("click", function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    video_tag.currentTime = 94;
+    video_tag.pause();
+  })
+
+  $(".menu .reception").bind("click", function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    video_tag.currentTime = 100;
+    video_tag.pause();
+  })
 });
