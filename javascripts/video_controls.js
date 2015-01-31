c427f899ee00c4b0e75c6f16c445122cc8892f4d
Hitesh Sharma <hitesh@slideshare.com>
2015-01-31 08:47:35 +0530

    progress bar

--- javascripts/video_controls.js
+++ javascripts/video_controls.js
@@ -15,4 +15,44 @@ $(document).ready(function(){
     video_tag.currentTime = seektime;
     video_tag.pause();
   });
+
+  $(video_tag).on("onwaiting", function(){
+    console.log("waiting");
+  });
+
+  $(video_tag).on("ended", function(){
+    console.log("ended");
+  });
+
+  $(video_tag).on("pause", function(){
+    console.log("paused");
+  });
+
+  $(video_tag).on("playing", function(){
+    console.log("playing");
+  });
+
+  $(video_tag).on("timeupdate", (function(){
+    var progress = $(".progress");
+    return function(e){
+      progress.animate({
+        width: this.currentTime*100/this.duration.toString() + "%",
+        duration: "fast"
+      });
+    };
+  })());
+
+  $(video_tag).on("error", function(e){
+    console.log("loadedata");
+  });
+
+  $(video_tag).on("loadeddata", function(e){
+    console.log("loadeddata");
+  });
+
+  $(video_tag).on("loadedmetadata", function(e){
+    console.log("loadedmetadata");
+  });
+
+  
 });
