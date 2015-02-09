27874a994a221d40c1a5004878dd072ec02f6517
Hitesh Sharma <hitesh@slideshare.com>
2015-02-09 15:21:31 +0530

    updating volume change time

--- javascripts/video_controls.js
+++ javascripts/video_controls.js
@@ -58,7 +58,7 @@ $(document).ready(function(){
     return function(e){
       progress.width((this.currentTime*100/this.duration).toString() + "%");
 
-      if(this.currentTime < 116){
+      if(this.currentTime < 117){
         this.volume = 0.15;
       } else {
         this.volume = 1;
